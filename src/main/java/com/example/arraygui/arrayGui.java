48e751cc19f700c0536939420f8f7f64fa3ea0ef
ia-usgs <irvin.i.avitia@gmail.com>
2023-07-17 12:18:06 -0700

    updated stuff

--- src/main/java/com/example/arraygui/arrayGui.java
+++ src/main/java/com/example/arraygui/arrayGui.java
@@ -11,7 +11,7 @@ public class arrayGui extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(arrayGui.class.getResource("scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 650);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 625);
         stage.setTitle("Array GUI Application");
         stage.setScene(scene);
         stage.show();
